663b9acdf478fdae4f116105df5917faf59b7968
Gerald Wagner <wagner.gerald.at@gmail.com>
2013-07-24 15:03:13 +0200

    added compiled files (Android apk)

---
+++ Examples/Android/trunk/FreeFlight2/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/gerald/ARDrone_SDK_2_0_1_new/Examples/Android/trunk/FreeFlight2/bin/AndroidManifest.xml : \
+/home/gerald/ARDrone_SDK_2_0_1_new/Examples/Android/trunk/FreeFlight2/AndroidManifest.xml \
