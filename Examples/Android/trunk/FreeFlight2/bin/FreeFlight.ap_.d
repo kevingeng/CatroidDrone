ac717e9366136579477767c346e15567a68af793
Gerald Wagner <wagner.gerald.at@gmail.com>
2013-08-07 13:48:35 +0200

    Undeleted fonts, created launcher for default app

--- Examples/Android/trunk/FreeFlight2/bin/FreeFlight.ap_.d
+++ Examples/Android/trunk/FreeFlight2/bin/FreeFlight.ap_.d
@@ -1764,4 +1764,6 @@
 /home/gerald/ARDrone_SDK_2_0_1_new/Examples/Android/trunk/FreeFlight2/bin/res/drawable-large-xhdpi/ff2_battery_080.png \
 /home/gerald/ARDrone_SDK_2_0_1_new/Examples/Android/trunk/FreeFlight2/bin/res/drawable-large-xhdpi/ff2_battery_060.png \
 /home/gerald/ARDrone_SDK_2_0_1_new/Examples/Android/trunk/FreeFlight2/bin/res/drawable-large-xhdpi/ff2_battery_100.png \
+/home/gerald/ARDrone_SDK_2_0_1_new/Examples/Android/trunk/FreeFlight2/assets/fonts \
+/home/gerald/ARDrone_SDK_2_0_1_new/Examples/Android/trunk/FreeFlight2/assets/fonts/helveticaneue-condensedbold.otf \
 /home/gerald/ARDrone_SDK_2_0_1_new/Examples/Android/trunk/FreeFlight2/bin/AndroidManifest.xml \
