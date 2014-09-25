3c409068db33e02b714f461dde33942cde4e767b
Markus Hobisch <hobisch@student.tugraz.at>
2014-09-25 18:56:57 +0200

    some experiments

--- CatroidDrone/src/com/parrot/freeflight/activities/SplashActivity.java
+++ CatroidDrone/src/com/parrot/freeflight/activities/SplashActivity.java
@@ -30,231 +30,221 @@ import com.parrot.freeflight.activities.base.ParrotActivity;
 import com.parrot.freeflight.catroid.R;
 import com.parrot.freeflight.utils.DeviceCapabilitiesUtils;
 
-public class SplashActivity
-        extends ParrotActivity
-        implements Runnable
-{
-    public static final int SPLASH_TIME = 4000;
-    public final static String VIDEO_FILE_NAME = "arfreeflight";
-    
-    private static final String TAG = SplashActivity.class.getSimpleName();
+public class SplashActivity extends ParrotActivity implements Runnable {
+	public static final int SPLASH_TIME = 4000;
+	public final static String VIDEO_FILE_NAME = "arfreeflight";
 
-    protected boolean playIntro = false;
+	private static final String TAG = SplashActivity.class.getSimpleName();
 
-    private Thread thread;
-    private boolean isActive;
-    private int timeElapsed = 0;
-    private LinearLayout splashBackgroundView;
-    private View videoContainer;
-    private VideoView videoView;
+	protected boolean playIntro = false;
 
+	private Thread thread;
+	private boolean isActive;
+	private int timeElapsed = 0;
+	private LinearLayout splashBackgroundView;
+	private View videoContainer;
+	private VideoView videoView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        
-        if (isFinishing()) {
-            return;
-        }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        // Requesting full screen without title
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		if (isFinishing()) {
+			return;
+		}
 
-        setContentView(R.layout.splash_screen);
-        
-        videoContainer = (View) findViewById(R.id.videoContainer);
-        videoView = (VideoView) findViewById(R.id.videoView);
-        if (videoView != null) {
-            if (rawResourceExists(VIDEO_FILE_NAME)) {
-                playIntro = true;
-                Uri video = Uri.parse("android.resource://" + getPackageName() + "/raw/" + VIDEO_FILE_NAME);
-                videoView.setVideoURI(video);
-            } else {
-                playIntro = false;
-            }
-        }
-        splashBackgroundView = (LinearLayout) findViewById(R.id.splash);
+		// Requesting full screen without title
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        isActive = true;
-        thread = new Thread(this);
-        thread.start();
-        
-        DeviceCapabilitiesUtils.dumpScreenSizeInDips(this);
-    }
-    
-    @SuppressLint("NewApi")
-    private void dumpVideoCapabilitiesInfo()
-    {
-        // Here we try to use different methods to determine the maximum video frame size
-        // that device supports
-        
-        Log.i(TAG, "=== DEVICE VIDEO SUPPORT ====>>>>>>>>>");
-        
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            Log.i(TAG, "Codecs available to the system: ");
-            for (int i=0; i<MediaCodecList.getCodecCount(); ++i) {
-                MediaCodecInfo info = MediaCodecList.getCodecInfoAt(i);
+		setContentView(R.layout.splash_screen);
 
-                String[] supportedTypes = info.getSupportedTypes();
-                StringBuilder supportedTypesBuilder = new StringBuilder();
-                
-                for (int j=0; j<supportedTypes.length; ++j) {
-                    supportedTypesBuilder.append(supportedTypes[j]);
-                    if (j<(supportedTypes.length - 1)) {
-                        supportedTypesBuilder.append(", ");
-                    }
-                }
-                
-                Log.i(TAG, info.getName() + " , supported types: " + supportedTypesBuilder.toString());;
-            }
-        }
-        
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            if (CamcorderProfile.hasProfile(CamcorderProfile.QUALITY_720P)) { 
-                Log.i(TAG, "Device supports HD video [720p]");
-            } else if (CamcorderProfile.hasProfile(CamcorderProfile.QUALITY_480P)){
-                Log.i(TAG, "Device supports regular video [480p]");
-            } else if (CamcorderProfile.hasProfile(CamcorderProfile.QUALITY_QVGA)) {
-                Log.i(TAG, "Device supports low quality video [240p]");
-            } else {
-                Log.w(TAG, "Can't determine video support of this device.");
-            }
-        }
-        
-        CamcorderProfile prof = CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH);
-        if (prof != null) {
-            Log.i(TAG, "Highest video frame size for this device is [" + prof.videoFrameWidth + ", " + prof.videoFrameHeight + "]");
-        } else {
-            Log.w(TAG, "Unable to determine highest possible video frame size.");
-        }
-        
-        Log.i(TAG, "<<<<<<<<<=== DEVICE VIDEO SUPPORT ===");
-    }
+		videoContainer = (View) findViewById(R.id.videoContainer);
+		videoView = (VideoView) findViewById(R.id.videoView);
+		if (videoView != null) {
+			if (rawResourceExists(VIDEO_FILE_NAME)) {
+				playIntro = true;
+				Uri video = Uri.parse("android.resource://" + getPackageName()
+						+ "/raw/" + VIDEO_FILE_NAME);
+				videoView.setVideoURI(video);
+			} else {
+				playIntro = false;
+			}
+		}
+		splashBackgroundView = (LinearLayout) findViewById(R.id.splash);
 
-    
-    @Override
-    public boolean onTouchEvent(MotionEvent event)
-    {
-        if (event.getAction() == MotionEvent.ACTION_UP) {
-            isActive = false;
-            return true;
-        }
+		isActive = true;
+		thread = new Thread(this);
+		thread.start();
 
-        return super.onTouchEvent(event);
-    }
+		DeviceCapabilitiesUtils.dumpScreenSizeInDips(this);
+	}
 
-    @Override
-    protected void onDestroy()
-    {
-        stopThreadAndJoin();
+	@SuppressLint("NewApi")
+	private void dumpVideoCapabilitiesInfo() {
+		// Here we try to use different methods to determine the maximum video
+		// frame size
+		// that device supports
 
-        super.onDestroy();
-    }
+		Log.i(TAG, "=== DEVICE VIDEO SUPPORT ====>>>>>>>>>");
 
-    @Override
-    protected void onPause()
-    {
-        super.onPause();
-        stopThreadAndJoin();
-    }
-    
-    
-    @Override
-    protected void onResume()
-    {
-        super.onResume();
-        dumpVideoCapabilitiesInfo();
-    }
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+			Log.i(TAG, "Codecs available to the system: ");
+			for (int i = 0; i < MediaCodecList.getCodecCount(); ++i) {
+				MediaCodecInfo info = MediaCodecList.getCodecInfoAt(i);
 
-    public void run()
-    {
-        try {
-            while (isActive && (timeElapsed < SPLASH_TIME)) {
-                Thread.sleep(100);
+				String[] supportedTypes = info.getSupportedTypes();
+				StringBuilder supportedTypesBuilder = new StringBuilder();
 
-                if (isActive) {
-                    timeElapsed += 100;
-                }
-            }
-        } catch (InterruptedException e) {
-            // do nothing
-        } finally {
-            if (playIntro) {
-                playIntro();
-            } else {
-                proceedToNextActivity();
-            }
-        }
-    }
+				for (int j = 0; j < supportedTypes.length; ++j) {
+					supportedTypesBuilder.append(supportedTypes[j]);
+					if (j < (supportedTypes.length - 1)) {
+						supportedTypesBuilder.append(", ");
+					}
+				}
 
-    private void stopThreadAndJoin()
-    {
-        isActive = false;
+				Log.i(TAG, info.getName() + " , supported types: "
+						+ supportedTypesBuilder.toString());
+				;
+			}
+		}
 
-        try {
-            if (thread != null) {
-                thread.join();
-            }
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			if (CamcorderProfile.hasProfile(CamcorderProfile.QUALITY_720P)) {
+				Log.i(TAG, "Device supports HD video [720p]");
+			} else if (CamcorderProfile
+					.hasProfile(CamcorderProfile.QUALITY_480P)) {
+				Log.i(TAG, "Device supports regular video [480p]");
+			} else if (CamcorderProfile
+					.hasProfile(CamcorderProfile.QUALITY_QVGA)) {
+				Log.i(TAG, "Device supports low quality video [240p]");
+			} else {
+				Log.w(TAG, "Can't determine video support of this device.");
+			}
+		}
 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+		CamcorderProfile prof = CamcorderProfile
+				.get(CamcorderProfile.QUALITY_HIGH);
+		if (prof != null) {
+			Log.i(TAG, "Highest video frame size for this device is ["
+					+ prof.videoFrameWidth + ", " + prof.videoFrameHeight + "]");
+		} else {
+			Log.w(TAG, "Unable to determine highest possible video frame size.");
+		}
 
-    private void playIntro()
-    {
-        runOnUiThread(new Runnable()
-        {
-            public void run()
-            {
-                splashBackgroundView.setVisibility(View.GONE);
-                videoContainer.setVisibility(View.VISIBLE);
+		Log.i(TAG, "<<<<<<<<<=== DEVICE VIDEO SUPPORT ===");
+	}
 
-                videoView.setOnTouchListener(new OnTouchListener()
-                {
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (event.getAction() == MotionEvent.ACTION_UP) {
+			isActive = false;
+			return true;
+		}
 
-                    public boolean onTouch(View v, MotionEvent event)
-                    {
-                        videoView.stopPlayback();
-                        videoView.setEnabled(false);
-                        videoContainer.setVisibility(View.GONE);
+		return super.onTouchEvent(event);
+	}
 
-                        startActivity(new Intent(SplashActivity.this, ConnectActivity.class));
-                        finish();
+	@Override
+	protected void onDestroy() {
+		stopThreadAndJoin();
 
-                        return true;
-                    }
-                });
+		super.onDestroy();
+	}
 
-                videoView.setOnCompletionListener(new OnCompletionListener()
-                {
+	@Override
+	protected void onPause() {
+		super.onPause();
+		stopThreadAndJoin();
+	}
 
-                    public void onCompletion(MediaPlayer mp)
-                    {
-                        proceedToNextActivity();
-                    }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		dumpVideoCapabilitiesInfo();
+	}
 
-                });
+	public void run() {
+		try {
+			while (isActive && (timeElapsed < SPLASH_TIME)) {
+				Thread.sleep(100);
 
-                videoView.start();
-            }
-        });
-    }
+				if (isActive) {
+					timeElapsed += 100;
+				}
+			}
+		} catch (InterruptedException e) {
+			// do nothing
+		} finally {
+			if (playIntro) {
+				playIntro();
+			} else {
+				proceedToNextActivity();
+			}
+		}
+	}
 
-    private void proceedToNextActivity()
-    {
-        if (videoContainer != null) {
-            videoContainer.setVisibility(View.GONE);
-        }
+	private void stopThreadAndJoin() {
+		isActive = false;
 
-        Intent dashboard = new Intent(SplashActivity.this, DashboardActivity.class);
-        startActivity(dashboard);
-    }
-    private boolean rawResourceExists(String theName)
-    {
-        int resId = this.getResources().getIdentifier("arfreeflight", "raw", this.getPackageName());
-        return resId != 0;
-    }
+		try {
+			if (thread != null) {
+				thread.join();
+			}
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void playIntro() {
+		runOnUiThread(new Runnable() {
+			public void run() {
+				splashBackgroundView.setVisibility(View.GONE);
+				videoContainer.setVisibility(View.VISIBLE);
+
+				videoView.setOnTouchListener(new OnTouchListener() {
+
+					public boolean onTouch(View v, MotionEvent event) {
+						videoView.stopPlayback();
+						videoView.setEnabled(false);
+						videoContainer.setVisibility(View.GONE);
+
+						startActivity(new Intent(SplashActivity.this,
+								ConnectActivity.class));
+						finish();
+
+						return true;
+					}
+				});
+
+				videoView.setOnCompletionListener(new OnCompletionListener() {
+
+					public void onCompletion(MediaPlayer mp) {
+						proceedToNextActivity();
+					}
+
+				});
+
+				videoView.start();
+			}
+		});
+	}
+
+	private void proceedToNextActivity() {
+		if (videoContainer != null) {
+			videoContainer.setVisibility(View.GONE);
+		}
+
+		Intent dashboard = new Intent(SplashActivity.this,
+				DashboardActivity.class);
+		startActivity(dashboard);
+	}
+
+	private boolean rawResourceExists(String theName) {
+		int resId = this.getResources().getIdentifier("arfreeflight", "raw",
+				this.getPackageName());
+		return resId != 0;
+	}
 }
--- CatroidDrone/src/com/parrot/freeflight/catroid/VideoActivity.java
+++ CatroidDrone/src/com/parrot/freeflight/catroid/VideoActivity.java
@@ -1,15 +1,63 @@
 package com.parrot.freeflight.catroid;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
 
-public class VideoActivity extends Activity {
+import com.parrot.freeflight.activities.SettingsDialog;
+import com.parrot.freeflight.activities.base.ParrotActivity;
+import com.parrot.freeflight.receivers.DroneBatteryChangedReceiver;
+import com.parrot.freeflight.receivers.DroneBatteryChangedReceiverDelegate;
+import com.parrot.freeflight.receivers.DroneCameraReadyActionReceiverDelegate;
+import com.parrot.freeflight.receivers.DroneCameraReadyChangeReceiver;
+import com.parrot.freeflight.receivers.DroneEmergencyChangeReceiver;
+import com.parrot.freeflight.receivers.DroneEmergencyChangeReceiverDelegate;
+import com.parrot.freeflight.receivers.DroneFlyingStateReceiver;
+import com.parrot.freeflight.receivers.DroneFlyingStateReceiverDelegate;
+import com.parrot.freeflight.receivers.DroneRecordReadyActionReceiverDelegate;
+import com.parrot.freeflight.receivers.DroneRecordReadyChangeReceiver;
+import com.parrot.freeflight.receivers.DroneVideoRecordStateReceiverDelegate;
+import com.parrot.freeflight.receivers.DroneVideoRecordingStateReceiver;
+import com.parrot.freeflight.receivers.WifiSignalStrengthChangedReceiver;
+import com.parrot.freeflight.receivers.WifiSignalStrengthReceiverDelegate;
+import com.parrot.freeflight.sensors.DeviceOrientationChangeDelegate;
+import com.parrot.freeflight.settings.ApplicationSettings.EAppSettingProperty;
+import com.parrot.freeflight.ui.HudViewController;
+import com.parrot.freeflight.ui.SettingsDialogDelegate;
+
+public class VideoActivity extends ParrotActivity implements
+		DeviceOrientationChangeDelegate, WifiSignalStrengthReceiverDelegate,
+		DroneVideoRecordStateReceiverDelegate,
+		DroneEmergencyChangeReceiverDelegate,
+		DroneBatteryChangedReceiverDelegate, DroneFlyingStateReceiverDelegate,
+		DroneCameraReadyActionReceiverDelegate,
+		DroneRecordReadyActionReceiverDelegate, SettingsDialogDelegate {
+
+	private WifiSignalStrengthChangedReceiver wifiSignalReceiver;
+	private DroneVideoRecordingStateReceiver videoRecordingStateReceiver;
+	private DroneEmergencyChangeReceiver droneEmergencyReceiver;
+	private DroneBatteryChangedReceiver droneBatteryReceiver;
+	private DroneFlyingStateReceiver droneFlyingStateReceiver;
+	private DroneCameraReadyChangeReceiver droneCameraReadyChangedReceiver;
+	private DroneRecordReadyChangeReceiver droneRecordReadyChangeReceiver;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_video);
+
+		wifiSignalReceiver = new WifiSignalStrengthChangedReceiver(this);
+		videoRecordingStateReceiver = new DroneVideoRecordingStateReceiver(this);
+		droneEmergencyReceiver = new DroneEmergencyChangeReceiver(this);
+		droneBatteryReceiver = new DroneBatteryChangedReceiver(this);
+		droneFlyingStateReceiver = new DroneFlyingStateReceiver(this);
+		droneCameraReadyChangedReceiver = new DroneCameraReadyChangeReceiver(
+				this);
+		droneRecordReadyChangeReceiver = new DroneRecordReadyChangeReceiver(
+				this);
+
+		HudViewController view = new HudViewController(this, false);
+		view.setCameraButtonEnabled(false);
+		view.setRecordButtonEnabled(false);
 	}
 
 	@Override
@@ -17,6 +65,75 @@ public class VideoActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.video, menu);
 		return true;
+	}
+
+	@Override
+	public void prepareDialog(SettingsDialog dialog) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onDismissed(SettingsDialog settingsDialog) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onOptionChangedApp(SettingsDialog dialog,
+			EAppSettingProperty property, Object value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onDroneRecordReadyChanged(boolean ready) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onCameraReadyChanged(boolean ready) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onDroneFlyingStateChanged(boolean flying) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onDroneBatteryChanged(int value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onDroneEmergencyChanged(int code) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onDroneRecordVideoStateChanged(boolean recording,
+			boolean usbActive, int remainingTime) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onWifiSignalStrengthChanged(int strength) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onDeviceOrientationChanged(float[] orientation,
+			float magneticHeading, int magnetoAccuracy) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
